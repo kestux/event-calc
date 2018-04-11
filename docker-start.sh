3283745d3ec345f93dcef1b19120616bba17bf70
kestux <kestutisk@studioskylab.com>
2018-04-12 00:16:00 +0100

    project setup

---
+++ docker-start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker pull connectmedica/php7.1-xdebug
+docker run -p 8080:80 -p 8800:8000 -p 3366:3306 -v ~/sandbox:/var/www/html -d --name=event-cal -w /var/www/html/event-cal connectmedica/php7.1-xdebug:latest
+docker exec -it event-cal /usr/local/bin/php bin/console server:start 0.0.0.0:8000
---
+++ docker-stop.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker exec -it event-cal /usr/local/bin/php bin/console
