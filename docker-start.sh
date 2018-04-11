b5b2fdd9c1e805d270701ed038a5d6149e0a9285
kestux <kestutisk@studioskylab.com>
2018-04-12 00:55:54 +0100

    install docker when starting container

--- docker-start.sh
+++ docker-start.sh
@@ -2,4 +2,5 @@
 
 docker pull connectmedica/php7.1-xdebug
 docker run -p 8080:80 -p 8800:8000 -p 3366:3306 -v ~/sandbox:/var/www/html -d --name=event-cal -w /var/www/html/event-cal connectmedica/php7.1-xdebug:latest
-docker exec -it event-cal /usr/local/bin/php bin/console server:start 0.0.0.0:8000
+docker exec -it event-cal composer install
+docker exec -it event-cal bin/console server:start 0.0.0.0:8000
